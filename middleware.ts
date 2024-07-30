ac4db10f50866070aca9d3640b9ff10003d06fa3
MrAHMED <chikhaoui084@gmail.com>
2024-07-30 03:17:07 -0700

    middleware v1.0.0

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,26 @@
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export default async function middleware(req: NextRequest) {}
+export default async function middleware(req: NextRequest) {
+  const user = { email: "sss" }
+
+  if (
+    !user &&
+    (req.nextUrl.pathname.startsWith("/my-orders") ||
+      req.nextUrl.pathname.startsWith("/checkout"))
+  ) {
+    return NextResponse.redirect(new URL("/login", req.url))
+  }
+
+  if (
+    (!user || (user && user.email !== process.env.ADMIN_EMAIL)) &&
+    req.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    return new NextResponse(null, {
+      status: 404,
+    })
+  }
+}
+
+export const config = {
+  matcher: ["/my-orders", "/checkout", "/dashboard"],
+}
