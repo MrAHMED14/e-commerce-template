079c61869ed465a26ce893fb536d67bec6b85bb3
MrAHMED <chikhaoui084@gmail.com>
2024-08-01 02:24:36 -0700

    middleware v2.0.0

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,14 @@
 import { NextRequest, NextResponse } from "next/server"
+import { getUser } from "./lib/actions/auth/action"
 
-export default async function middleware(req: NextRequest) {
-  const user = { email: "sss" }
+export default async function middleware(request: NextRequest) {
+  const user = await getUser()
+  const authCookie = request.cookies.get("auth_session")
 
-  if (
-    !user &&
-    (req.nextUrl.pathname.startsWith("/my-orders") ||
-      req.nextUrl.pathname.startsWith("/checkout"))
-  ) {
-    return NextResponse.redirect(new URL("/login", req.url))
-  }
-
-  if (
-    (!user || (user && user.email !== process.env.ADMIN_EMAIL)) &&
-    req.nextUrl.pathname.startsWith("/dashboard")
-  ) {
-    return new NextResponse(null, {
-      status: 404,
-    })
-  }
+  if (!authCookie || !user)
+    return NextResponse.redirect(new URL("/login", request.url))
 }
 
 export const config = {
-  matcher: ["/my-orders", "/checkout", "/dashboard"],
+  matcher: ["/my-orders", "/checkout"],
 }
